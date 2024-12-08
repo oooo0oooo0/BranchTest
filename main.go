d2783ba1b0663dc8733cceb0640ec40c80638d5c
guangfa <oooo0oooo0@outlook.com>
2024-12-08 16:59:16 +0800

    b-1

--- main.go
+++ main.go
@@ -43,7 +43,7 @@ func init() {
 	rootCmd.AddCommand(migrate.StartCmd)
 	rootCmd.AddCommand(webhook.WebhookCmd)
 
-	fmt.Println("fobrain-feat/a")
+	fmt.Println("fobrain-feat/b")
 }
 
 // Execute : apply commands
